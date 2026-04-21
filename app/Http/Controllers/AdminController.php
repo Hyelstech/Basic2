dea326d1e7f76d82ce14340ce0394ca1b92a6028
Hyelstech <62312122+Hyelstech@users.noreply.github.com>
2026-04-21 01:35:03 +0100

    initial commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\VerificationCodeMail;
+
+class AdminController extends Controller
+{
+   public function AdminLogout(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    }
+    public function AdminLogin(Request $request)
+    {
+        $credentials = $request->only('email','password');
+        if (Auth::attempt($credentials))
+            {
+            $user = Auth::user();
+
+            $verificationCode = random_int(100000,999999);
+            session(['verification_code' => $verificationCode, 'user_id' => $user->id ]);
+
+            mail::to($user->email)->send(new VerificationCodeMail($verificationCode));
+            Auth::logout();
+            return redirect()->route('custom.verification.form')->with
+            ('status','Verification code sent to your mail');
+    
+            }
+        return redirect()->back()->witherrors(['email' => 'Invalid credentials provided']);
+    }
+    public function ShowVerification()
+    {
+        return view('auth.verify');
+    }
+        public function VerificationVerify(Request $request)
+        {
+
+        }
+}
