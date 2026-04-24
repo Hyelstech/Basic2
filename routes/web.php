1a0d88bd72a29780168c50092a3f383fc230038d
Hyelstech <62312122+Hyelstech@users.noreply.github.com>
2026-04-24 14:54:56 +0100

    Initial Laravel project

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('admin.index');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
+    
+     Route::post('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
+
+    Route::get('/verify', [AdminController::class, 'ShowVerification'])->name('custom.verification.form');
+
+    Route::post('/verify', [AdminController::class, 'VerificationVerify'])->name('custom.verification.form');
+
